8fae39c85b21e55e7b09c2b2f9df9dd653f19c8b
k-hatano <kent.ruffle.mgj626@gmail.com>
2022-02-03 01:16:49 +0900

    Implement

--- script.js
+++ script.js
@@ -2,103 +2,308 @@
 const singleSize = 32;
 
 const width = 20;
-const height = 10;
+const height = 12;
 
-const canvasWidth = width * singleSize;
-const canvasHeight = height * singleSize;
+const boardCanvasWidth = width * singleSize;
+const boardCanvasHeight = height * singleSize;
 
-let board = new Array();
+let pieceImage = new Image();
+let boardArray = new Array();
+let highlightedArray = new Array();
+let undoBoardArrays = new Array();
+let situation = 0; // 0: Playable, 1: No Move, 2: Perfect
 
+let lastMoveX = -1;
+let lastMoveY = -1;
 
 onload = function() {
     init();
-    randomize();
-    drawCanvas();
+    loadImage();
+    newGame();
 
-    let canvas = document.getElementById("canvas");
-    canvas.onmousedown = function(event) {
-        canvasMouseDown(event);
-    };
+    let boardCanvas = document.getElementById("board");
+    boardCanvas.addEventListener('mousedown', function(event) {
+        boardCanvasMouseDown(event);
+    });
+    boardCanvas.addEventListener('mousemove', function(event) {
+        boardCanvasMouseMove(event);
+    });
+    boardCanvas.addEventListener('mouseout', function(event) {
+        boardCanvasMouseOut(event);
+    });
 
     document.getElementById('new').addEventListener('click', function(event){
-        randomize();
-        drawCanvas();
+        newGame(event.altKey);
+    });
+
+    document.getElementById('undo').addEventListener('click', function(event){
+        undo();
+    });
+
+    document.getElementById('retry').addEventListener('click', function(event){
+        retry();
+    });
+
+    document.getElementById('find').addEventListener('click', function(event){
+        findAll();
     });
 };
 
-function init() {
-    board = new Array(width);
-    for (let i = 0; i < width; i++) {
-        board[i] = new Array(height);
+function hightlightIfAbleToVanish(x, y) {
+    if (boardArray[x][y] == 0) {
+        return false;
     }
+    if (x < 0 || x >= width || y < 0 || y >= height) {
+        return false;
+    }
+    if (highlightedArray[x][y] == 1) {
+        return false;
+    }
+
+    if (x > 0 && boardArray[x][y] == boardArray[x - 1][y]) {
+        highlightedArray[x][y] = 1;
+        hightlightIfAbleToVanish(x - 1, y);
+    }
+    if (x < width - 1 && boardArray[x][y] == boardArray[x + 1][y]) {
+        highlightedArray[x][y] = 1;
+        hightlightIfAbleToVanish(x + 1, y);
+    }
+    if (y > 0 && boardArray[x][y] == boardArray[x][y - 1]) {
+        highlightedArray[x][y] = 1;
+        hightlightIfAbleToVanish(x, y - 1);
+    }
+    if (y < height - 1 && boardArray[x][y] == boardArray[x][y + 1]) {
+        highlightedArray[x][y] = 1;
+        hightlightIfAbleToVanish(x, y + 1);
+    }
+}
+
+function boardCanvasMouseMove(event) {
+    let x = Math.floor(event.offsetX / singleSize);
+    let y = Math.floor(event.offsetY / singleSize);
+    if (x == lastMoveX && y == lastMoveY) {
+        return;
+    }
+    resetHighlight();
+    hightlightIfAbleToVanish(x, y);
+    drawCanvas();
+    lastMoveX = x;
+    lastMoveY = y;
+}
+
+function boardCanvasMouseOut(event) {
+    lastMoveX = -1;
+    lastMoveY = -1;
+    resetHighlight();
+    drawCanvas();
+}
+
+function resetHighlight() {
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            highlightedArray[x][y] = 0;
+        }
+    }
+}
+
+function findAll() {
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            if (ableToVanish(x, y)) {
+                highlightedArray[x][y] = 1;
+            } else {
+                highlightedArray[x][y] = 0;
+            }
+        }
+    }
+    drawCanvas();
+}
+
+function unfind() {
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            highlightedArray[x][y] = 0;
+        }
+    }
+}
+
+function init() {
+    boardArray = new Array(width);
+    highlightedArray = new Array(width);
+    for (let i = 0; i < width; i++) {
+        boardArray[i] = new Array(height);
+        highlightedArray[i] = new Array(height);
+    }
+    undoBoardArrays = new Array();
+}
+
+function newGame(magic) {
+    if (magic) {
+        tidy();
+    } else {
+        randomize();
+    }
+    drawCanvas();
+    updateCountLabels();
+}
+
+function undo() {
+    let lastUndoBoardArray = undoBoardArrays.pop();
+    if (lastUndoBoardArray != undefined) {
+        situation = 0;
+        boardArray = JSON.parse(lastUndoBoardArray);
+        drawCanvas();
+        updateCountLabels();
+    }
+}
+
+function retry() {
+    if (undoBoardArrays.length >= 1) {
+        situation = 0;
+        boardArray = JSON.parse(undoBoardArrays[0]);
+        undoBoardArrays = new Array();
+        drawCanvas();
+        updateCountLabels();
+    }
+}
+
+function updateCountLabels() {
+    let counts = [0, 0, 0, 0, 0, 0];
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            counts[boardArray[x][y]]++;
+        }
+    }
+
+    for (let i = 1; i <= 5; i++) {
+        let countLabelName = "count_" + i;
+        let countLabel = document.getElementById(countLabelName);
+        countLabel.innerText = "" + counts[i];
+    }
+}
+
+function loadImage() {
+    pieceImage = new Image();
+    pieceImage.src = ("./marble.png");
+    pieceImage.onload = function() {
+        drawCanvas();
+    };
+    pieceImage.onerror = function() {
+        drawCanvas();
+    };
+}
+
+function tidy() {
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            boardArray[x][y] = (x % 5) + 1;
+            highlightedArray[x][y] = 0;
+        }
+    }
+    undoBoardArrays = new Array();
+    situation = 0;
 }
 
 function randomize() {
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            board[x][y] = Math.floor(Math.random() * 5) + 1;
+            boardArray[x][y] = Math.floor(Math.random() * 5) + 1;
+            highlightedArray[x][y] = 0;
         }
     }
+    undoBoardArrays = new Array();
+    situation = 0;
 }
 
 function drawCanvas() {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvasWidth, canvasHeight);
+    for (let i = 1; i <= 5; i++) {
+        let pieceCanvas = document.getElementById('piece_' + i);
+        let pieceContext = pieceCanvas.getContext('2d');
+        pieceContext.drawImage(pieceImage, (i - 1) * singleSize, 0, singleSize, singleSize, 0, 0, 22, 22); 
+    }
+
+    var boardCanvas = document.getElementById('board');
+    var boardContext = boardCanvas.getContext('2d');
+    boardContext.clearRect(0, 0, boardCanvasWidth, boardCanvasHeight);
+
+    boardContext.fillStyle = "white";
+    boardContext.fillRect(0, 0, boardCanvasWidth, boardCanvasHeight);
 
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            switch (board[x][y]) {
+            let highlight = highlightedArray[x][y];
+            switch (boardArray[x][y]) {
                 case 1:
-                    context.fillStyle = "red";
+                    boardContext.drawImage(pieceImage, 0 * singleSize, highlight * singleSize, singleSize, singleSize,
+                        x * singleSize, y * singleSize, singleSize, singleSize);
                     break;
                 case 2:
-                    context.fillStyle = "green";
+                    boardContext.drawImage(pieceImage, 1 * singleSize, highlight * singleSize, singleSize, singleSize,
+                        x * singleSize, y * singleSize, singleSize, singleSize);
                     break;
                 case 3:
-                    context.fillStyle = "blue";
+                    boardContext.drawImage(pieceImage, 2 * singleSize, highlight * singleSize, singleSize, singleSize,
+                        x * singleSize, y * singleSize, singleSize, singleSize);
                     break;
                 case 4:
-                    context.fillStyle = "orange";
+                    boardContext.drawImage(pieceImage, 3 * singleSize, highlight * singleSize, singleSize, singleSize,
+                        x * singleSize, y * singleSize, singleSize, singleSize);
                     break;
                 case 5:
-                    context.fillStyle = "purple";
+                    boardContext.drawImage(pieceImage, 4 * singleSize, highlight * singleSize, singleSize, singleSize,
+                        x * singleSize, y * singleSize, singleSize, singleSize);
                     break;
                 default:
-                    context.fillStyle = "white";
                     break;
             }
-            context.fillRect(x * singleSize, y * singleSize, (x + 1) * singleSize, (y + 1) * singleSize);
         }
     }
 
+    if (situation == 1) { // No Move
+        boardContext.font = "36px sans-serif";
+        boardContext.strokeStyle = "black";
+        boardContext.fillStyle = "black";
+        boardContext.textAlign = "center";
+        boardContext.fillText("No Move", boardCanvasWidth / 2, boardCanvasHeight * 1 / 4);
+    }
+    if (situation == 2) { // Prefect
+        boardContext.font = "36px sans-serif";
+        boardContext.strokeStyle = "black";
+        boardContext.fillStyle = "black";
+        boardContext.textAlign = "center";
+        boardContext.fillText("Perfect!", boardCanvasWidth / 2, boardCanvasHeight * 1 / 4);
+    }
 }
 
-function canvasMouseDown(event) {
+function boardCanvasMouseDown(event) {
     let x = Math.floor(event.offsetX / singleSize);
     let y = Math.floor(event.offsetY / singleSize);
     if (ableToVanish(x, y)) {
-        vanish(x, y, board[x][y]);
+        undoBoardArrays.push(JSON.stringify(boardArray));
+        vanish(x, y, boardArray[x][y]);
         grave();
+        updateCountLabels();
     }
+    situation = checkSituation();
+    unfind();
     drawCanvas();
 }
 
 function ableToVanish(x, y) {
-    if (board[x][y] == 0) {
+    if (boardArray[x][y] == 0) {
         return false;
     }
 
-    if (x > 0 && board[x][y] == board[x - 1][y]) {
+    if (x > 0 && boardArray[x][y] == boardArray[x - 1][y]) {
         return true;
     }
-    if (y > 0 && board[x][y] == board[x][y - 1]) {
+    if (y > 0 && boardArray[x][y] == boardArray[x][y - 1]) {
         return true;
     }
-    if (x < width - 1 && board[x][y] == board[x + 1][y]) {
+    if (x < width - 1 && boardArray[x][y] == boardArray[x + 1][y]) {
         return true;
     }
-    if (y < height - 1 && board[x][y] == board[x][y + 1]) {
+    if (y < height - 1 && boardArray[x][y] == boardArray[x][y + 1]) {
         return true;
     }
     return false;
@@ -108,10 +313,10 @@ function vanish(x, y, color) {
     if (x < 0 || x >= width || y < 0 || y >= height) {
         return;
     }
-    if (board[x][y] != color) {
+    if (boardArray[x][y] != color) {
         return;
     }
-    board[x][y] = 0;
+    boardArray[x][y] = 0;
     vanish(x - 1, y, color);
     vanish(x + 1, y, color);
     vanish(x, y - 1, color);
@@ -121,9 +326,9 @@ function vanish(x, y, color) {
 function grave() {
     for (x = 0; x < width; x++) {
         for (y = height - 1; y > 0; y--) {
-            if (board[x][y] == 0 && board[x][y - 1] != 0) {
-                board[x][y] = board[x][y - 1];
-                board[x][y - 1] = 0;
+            if (boardArray[x][y] == 0 && boardArray[x][y - 1] != 0) {
+                boardArray[x][y] = boardArray[x][y - 1];
+                boardArray[x][y - 1] = 0;
 
                 y = height;
             }
@@ -133,24 +338,53 @@ function grave() {
     for (x = 0; x < width - 1; x++) {
         let rowIsBlank = true;
         for (y = 0; y < height; y++) {
-            if (board[x][y] != 0) {
+            if (boardArray[x][y] != 0) {
                 rowIsBlank = false;
                 break;
             }
         }
         let nextRowIsBlank = true;
         for (y = 0; y < height; y++) {
-            if (board[x + 1][y] != 0) {
+            if (boardArray[x + 1][y] != 0) {
                 nextRowIsBlank = false;
                 break;
             }
         }
         if (rowIsBlank && !nextRowIsBlank) {
             for (y = 0; y < height; y++) {
-                board[x][y] = board[x + 1][y];
-                board[x + 1][y] = 0;
+                boardArray[x][y] = boardArray[x + 1][y];
+                boardArray[x + 1][y] = 0;
             }
             x = -1;
         }
+    }
+}
+
+function checkSituation() {
+    let remains = 0;
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            if (boardArray[x][y] == 0) {
+                continue;
+            }
+            remains++;
+            if (x > 0 && boardArray[x][y] == boardArray[x - 1][y]) {
+                return 0;
+            }
+            if (x < width - 1 && boardArray[x][y] == boardArray[x + 1][y]) {
+                return 0;
+            }
+            if (y > 0 && boardArray[x][y] == boardArray[x][y - 1]) {
+                return 0;
+            }
+            if (y < height - 1 && boardArray[x][y] == boardArray[x][y + 1]) {
+                return 0;
+            }
+        }
+    }
+    if (remains == 0) {
+        return 2; // Perfect
+    } else {
+        return 1; // No Move
     }
 }
